d903b78018547578ebce2c8ae0a717c29f4ddde0
Aleksey Udod <lioshkaudod@yandex.ru>
2026-06-17 17:34:03 +0400

    feat: добавлены Page Object, тесты, локаторы, константы URL

--- tests/test_logo_redirect.py
+++ tests/test_logo_redirect.py
@@ -6,13 +6,11 @@ from page_objects.main_page import MainPage
 
 class TestLogoRedirect:
     @allure.title('Проверка перехода на главную страницу сервиса при клике на лого "Самокат" в шапке')
-    def test_logo_redirect_to_main_page_success(self, driver):
+    def test_logo_redirect_to_main_success(self, driver):
         main_page = MainPage(driver)
-        main_page.wait_visibility_of_order_button_in_header()
-        main_page.click_on_order_button_in_header()
         main_page.wait_visibility_of_header_logo_scooter()
         main_page.click_on_header_logo_scooter()
-        main_page.wait_visibility_of_main_header()
+        # Одним вызовом ждём и проверяем
         assert main_page.check_displaying_of_main_header()
 
     @allure.title('Проверка перехода на страницу "Дзена" при клике на лого "Яндекс"')
